3b302dedb60427038e246a182aea25f23f8e2fbc
markkaytavion <ferguson.devontae@gmail.com>
2022-07-14 22:31:16 -0400

    Basic nav component with bootstrap

--- luv-uv-lyric/src/App.js
+++ luv-uv-lyric/src/App.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
-
+import './App.css'
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Index from "./components/Index";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Luv Uv Lyric</h1>
-    </div>
+    <Router>
+    <React.Fragment>
+      <Navbar />
+        <div className="container">
+          
+            <Route exact path="/" component={Index} />
+          
+        </div>
+      </React.Fragment>
+    </Router>
   );
 }
 
