3411ae0683eab73cb2bcb9da4e86bdb8b3f8ddc2
markkaytavion <ferguson.devontae@gmail.com>
2022-07-18 16:32:37 -0400

    stored api key in env

--- luv-uv-lyric/src/context.js
+++ luv-uv-lyric/src/context.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 
 const Context = React.createContext();
 
 export class Provider extends Component {
     state = {
-        track_list: [
-            { track: { track_name:'abc'} },
-            { track: { track_name:'123'} }
-        ],
+        track_list: [],
         heading: 'Top 10 Lyrics'
     }
 
     componentDidMount(){
-        
+      axios.get()
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));  
+
     }
 
     render() {
