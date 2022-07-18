6afc24aa08301afcaed7b7747481a6010cd921cc
markkaytavion <ferguson.devontae@gmail.com>
2022-07-18 15:22:53 -0400

    Switching

--- luv-uv-lyric/src/App.js
+++ luv-uv-lyric/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './App.css'
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Index from "./components/Index";
 import Tracks from "./components/Tracks";
 
